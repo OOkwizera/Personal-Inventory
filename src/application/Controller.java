a8432e4916c4bfa1ca41e79d5db9b41a7e55f5ec
CandyceSarringar <sarringarcd@hendrix.edu>
2015-10-31 21:16:17 -0500

    Added the database language to project.

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -3,5 +3,5 @@ package application;
 public class Controller {
   //for interacting with the GUI
 	
-	//need get-er and set-er methods to recieve info from the text boxes.
+	//need get-er and set-er methods to receive info from the text boxes.
 }
